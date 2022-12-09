ade8e162289fbdbfaddb7dc422b3461f04126449
xSlendiX <slendi@socopon.com>
2022-12-09 15:06:36 +0200

    Add github action.
    Signed-off-by: xSlendiX <slendi@socopon.com>

---
+++ .github/build.sh
@@ -0,0 +1,11 @@
+alias arduino="arduino-cli --additional-urls 'https://github.com/earlephilhower/arduino-pico/releases/download/global/package_rp2040_index.json'"
+
+arduino-cli core update-index
+
+arduino core install 'rp2040:rp2040'
+arduino board attach -b 'rp2040:rp2040:rpipico' --board-options 'flash=2097152_1048576,usbstack=tinyusb'
+arduino lib install 'Adafruit SPIFlash'
+arduino config set sketch.always_export_binaries true
+
+arduino compile -b 'rp2040:rp2040:rpipico' -e --board-options 'flash=2097152_1048576,usbstack=tinyusb'
+
